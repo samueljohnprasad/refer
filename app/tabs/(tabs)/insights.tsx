54e7f8790cc7933cf232af8c626d72bd389eb916
samueljohnprasad <samgolla.johnprasad777@gmail.com>
2025-12-06 18:55:27 +0530

    tab

--- app/tabs/(tabs)/insights.tsx
+++ app/tabs/(tabs)/insights.tsx
@@ -1,10 +1,13 @@
 import SuspensLoader from "@/src/components/SuspensLoader";
-import SwiftUI from "@/src/components/ui/swiftui";
 import React, { lazy } from "react";
 const AIInsightsScreen = lazy(
   () => import("@/src/screens/AIInsightsScreen/AIInsightsScreen")
 );
 
 export default function InsightsTab() {
-  return <SwiftUI />;
+  return (
+    <SuspensLoader>
+      <AIInsightsScreen />
+    </SuspensLoader>
+  );
 }
